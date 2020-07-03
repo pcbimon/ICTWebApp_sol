c2e1953df4fb65e732c728c5d8e874aaa5d48308
Patipat Chewprecha <patipat.che@mahidol.edu>
2020-07-03 11:35:06 +0700

    get data from db

--- ICTWebApp/Controllers/CategoryController.cs
+++ ICTWebApp/Controllers/CategoryController.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ICTWebApp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ICTWebApp.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var category = await _context.Categories.ToListAsync();
+            return View(category);
         }
 
         public IActionResult Create()
