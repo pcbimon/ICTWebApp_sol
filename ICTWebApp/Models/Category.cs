cada5cef20cbb396e9bf5a260eb13b209185540f
Patipat Chewprecha <patipat.che@mahidol.edu>
2020-07-03 11:08:46 +0700

    Add migration and update to db

---
+++ ICTWebApp/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICTWebApp.Models
+{
+    //Mapping to Table
+    //[Table("category")]
+    public class Category
+    {
+        //Set as Primary Key
+        //[Key]
+        //Match to Column
+        //[Column("cat_id")]
+        //Set Identity
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryID { get; set; }
+        //Set Column Type
+        //[Column(TypeName = "nText")]
+        public string CategoryName { get; set; }
+    }
+}
